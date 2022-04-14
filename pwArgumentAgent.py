fd7e33417b36511a45bccc577fb482714bdfd200
Morgane-SJK <senejko.morgane@gmail.com>
2022-04-14 18:03:17 +0200

    refact : create specific functions to handle different type of messages

--- pwArgumentAgent.py
+++ pwArgumentAgent.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 import pandas as pd
 from mesa import Model
@@ -25,9 +26,7 @@ class ArgumentAgent(CommunicatingAgent):
         self.arguments: dict[str,Argument] = {}
         self.counter_arguments = {}
         self.already_proposed = []
-        # diesel_engine = Item("Diesel Engine", "A super cool diesel engine")
-        # electric_engine = Item("Electric Engine", "A very quiet engine")
-        # self.item_list = [diesel_engine, electric_engine]
+        self.number_commit = 0
 
     def step(self):
         super().step()
@@ -39,84 +38,37 @@ class ArgumentAgent(CommunicatingAgent):
                                       self.preference.most_preferred(self.item_list)))
 
         for message in list_messages:
+
             print(message)
-            content = message.get_content()
+            content = message.get_content() #content of the message
 
             if message.get_performative() == MessagePerformative.PROPOSE:
-                print("\n-----------------------------------------PROPOSE-----------------------------------------")
-
-                if self.preference.is_item_among_top_10_percent(content, self.item_list):
-
-                    if (content.get_name() == self.preference.most_preferred(self.item_list).get_name()):
-
-                        self.send_message(
-                            Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, content))
-
-                    else:
-                        print(
-                            f"{self.get_name()} propose something else : {self.preference.most_preferred(self.item_list)}")
-                        self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.PROPOSE,
-                                                  self.preference.most_preferred(self.item_list)))
-
-
-
-                else:
-                    self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ASK_WHY, content))
+                self.handle_propose(message, content)
 
             if message.get_performative() == MessagePerformative.ACCEPT:
-                print("\n-----------------------------------------ACCEPT-----------------------------------------")
                 self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.COMMIT, content))
 
             if message.get_performative() == MessagePerformative.COMMIT:
-                print("\n-----------------------------------------COMMIT-----------------------------------------")
-                if content.get_name() in [it.get_name() for it in self.item_list]:
-                    self.item_list = list(filter(lambda x: x.get_name() != content.get_name(), self.item_list))
-                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.COMMIT, content))
+                self.handle_commit(message, content)
 
             if message.get_performative() == MessagePerformative.ASK_WHY:
-                print("\n-----------------------------------------ASK WHY-----------------------------------------")
-
-                current_item = [item for item in self.item_list if item.get_name() == content.get_name()][0].get_name()
-                if self.support_proposal(current_item) == None:
-                    print(
-                        f"{self.get_name()} propose something else: {self.preference.most_preferred(self.item_list)}")
-                    unproposed_best_items = [item for item in self.preference.get_item_sorted_by_score(self.item_list)
-                                             if item not in self.already_proposed]
-                    if len(unproposed_best_items) > 0:
-                        self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.PROPOSE,
-                                                  unproposed_best_items[0]))
-                    else:
-                        print("On a pass")
-                        pass
-                else:
-                    self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ARGUE,(current_item,
-                                              self.argument_parsing(self.support_proposal(current_item)))))
-                    print(str(self.arguments['Electric Engine'].couple_values_list))
+                self.handle_askwhy(message, content)
 
             if message.get_performative() == MessagePerformative.ARGUE:
-                print("\n-----------------------------------------ARGUE-----------------------------------------")
-                item, criterion_couple = content
-                if self.is_argument_attack(item, criterion_couple):
-                    if self.attack_argument(item) == None:
-                        self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, item))
-                    else:
-                        print("TODO")
-                        #self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ARGUE, (item,)))
-                else:
-                    self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, item))
+                self.handle_argue(message, content)
 
-                # TODO : implement the argument agent
 
     def get_preference(self):
         return self.preference
 
-    def generate_preferences(self, item_list):
+    def generate_preferences(self):
+        """Generate the preferences of an agent given a CSV file containing the value associated for each criterion on an item"""
         df = pd.read_csv(f"data/{self.get_name()}.csv", index_col=0, sep=";")
         agent_pref = Preferences()
         agent_pref.set_criterion_name_list([CriterionName[col] for col in df.columns])
         items = []
         for index, data in df.iterrows():
-            item = Item(index, "Awesome description")
+            item = Item(index, "")
             items.append(item)
 
             for row in data.iteritems():
@@ -129,9 +81,10 @@ class ArgumentAgent(CommunicatingAgent):
         """
         Used when the agent receives " ASK_WHY " after having proposed an item
         : param item : str - name of the item which was proposed
-        : return : Argument - the strongest supportive argument
+        : return : Argument - an argument object which contains the list of all the arguments available to defend the given item
         """
 
+        #if we never created argument for this item, we create it
         if item not in self.arguments:
             argument = Argument(True, item)
             supportive_argument = argument.list_supporting_proposal(item, self.get_preference())
@@ -139,14 +92,20 @@ class ArgumentAgent(CommunicatingAgent):
                 argument.add_premiss_couple_values(criterion.name, value.name)
             self.arguments[item] = argument
 
+        #if we still have arguments left in our list 
         if len(self.arguments[item].couple_values_list) > 0:
             return self.arguments[item]
 
         return None
 
-    def argument_parsing(self, argument):
+    def argument_parsing(self, argument:Argument):
+        """Parse an argument and return the strongest one"""
 
-        criterion = self.arguments[argument.item].couple_values_list.pop(0)
+        #if it's a counter argument
+        if argument.boolean_decision == False:
+            criterion = self.counter_arguments[argument.item].couple_values_list.pop(0)
+        else:
+            criterion = self.arguments[argument.item].couple_values_list.pop(0)
 
         #print(f"{self.arguments[argument.item][0].item}==>{criterion.criterion_name} = {criterion.value}")
 
@@ -168,14 +127,91 @@ class ArgumentAgent(CommunicatingAgent):
         return None
 
     def is_argument_attack(self, item, criterion_couple_values):
+        """Boolean which indicates if an argument can be attacked or not and fill the counter arguments"""
         criterion, value = criterion_couple_values
 
-        importance = self.preference.get_criterion_name_list().index(criterion)
+        importance = [crit.name for crit in self.preference.get_criterion_name_list()].index(criterion)
+        #if the criterion is not important for him (not in the 50% preferred criterion)
         if importance > len(self.preference.get_criterion_name_list()) / 2:
             return False
-        if value > self.preference.get_value(item, criterion):
+
+        #if its value for the given criterion is less than the one of the agent
+        if Value[value].value > self.preference.get_value(item, CriterionName[criterion]).value:
             return False
         return True
+
+
+    def handle_propose(self, message, content):
+        """Handle the messages of type PROPOSE"""
+
+        #if the proposed item is among the top 10 percent of the agent who receives the proposal
+        if self.preference.is_item_among_top_10_percent(content, self.item_list):
+
+            #if the proposed item is the favorite item of the agent who receives the proposal -> he accepts the item
+            if (content.get_name() == self.preference.most_preferred(self.item_list).get_name()):
+
+                self.send_message(
+                    Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, content))
+
+            #else he proposes something else
+            else:
+                print(f"{self.get_name()} propose something else : {self.preference.most_preferred(self.item_list)}")
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.PROPOSE,
+                                            self.preference.most_preferred(self.item_list)))
+
+        else:
+            self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ASK_WHY, content))
+
+    def handle_commit(self, message, content):
+        """Handle the messages of type COMMIT"""
+
+        item_name = content if isinstance(content,str) else content.get_name()
+        if item_name in [it.get_name() for it in self.item_list]:
+            self.item_list = list(filter(lambda x: x.get_name() != item_name, self.item_list))
+        self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.COMMIT, content))
+        self.number_commit +=1
+
+        #if both the agents commited, we end the negotiation
+        if self.number_commit == 2:
+            print(f"\nEnd of the negociation ! The agents agreed on the item : {item_name}")
+            sys.exit()
+
+    def handle_askwhy(self, message, content):
+        """Handle the messages of type ASK_WHY"""
+
+        current_item = [item for item in self.item_list if item.get_name() == content.get_name()][0].get_name()
+        if self.support_proposal(current_item) == None:
+            print(
+                f"{self.get_name()} propose something else: {self.preference.most_preferred(self.item_list)}")
+            unproposed_best_items = [item for item in self.preference.get_item_sorted_by_score(self.item_list)
+                                        if item not in self.already_proposed]
+            if len(unproposed_best_items) > 0:
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.PROPOSE,
+                                            unproposed_best_items[0]))
+            else:
+                pass
+        else:
+            self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ARGUE,(current_item,
+                                        self.argument_parsing(self.support_proposal(current_item)))))
+            print(str(self.arguments['Electric Engine'].couple_values_list))
+
+    def handle_argue(self, message, content):
+        """Handle the messages of type ARGUE"""
+        item, criterion_couple = content
+
+        #if the argument can be attacked by the agent
+        if self.is_argument_attack(item, criterion_couple):
+            #if we don't have counter arguments -> we accept the item
+            if self.attack_argument(item, criterion_couple) == None:
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, item))
+            #we give a counter argument
+            else:
+                self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ARGUE, (item, self.argument_parsing(self.attack_argument(item, criterion_couple)))))
+        
+        #the argument can't be attacked by the agent -> he accepts
+        else:
+            self.send_message(Message(self.get_name(), message.get_exp(), MessagePerformative.ACCEPT, item))
+
 
 
 class ArgumentModel(Model):
@@ -207,22 +243,16 @@ if __name__ == '__main__':
     MessageService.get_instance().set_instant_delivery(True)
 
     alice = ArgumentAgent(0, argument_model, "Alice")
-    alice.generate_preferences([])
+    alice.generate_preferences()
     argument_model.schedule.add(alice)
 
 
     bob = ArgumentAgent(1, argument_model, "Bob")
-    bob.generate_preferences([])
+    bob.generate_preferences()
     argument_model.schedule.add(bob)
 
-# m1 = Message("Alice", "Bob", MessagePerformative.PROPOSE, "Tu veux manger ?")
-    # m2 = Message("Bob", "Alice", MessagePerformative.ACCEPT, "Evidemment Bregs")
-    #
-    # alice.send_message(m1)
-    # bob.send_message(m2)
     step = 0
     while step < 10:
+        print("step=",step)
         argument_model.step()
         step += 1
-
-# To be completed
