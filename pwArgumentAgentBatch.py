a433642245183a17f5978499b425c918ddacec02
alexfrst <alex.forest1026@gmail.com>
2022-04-15 20:29:37 +0200

    feat: updated graphs

--- pwArgumentAgentBatch.py
+++ pwArgumentAgentBatch.py
@@ -20,14 +20,14 @@ item_names = [
 
 agents_couple = ([(a1,a2) for a1, a2 in product(player_names, player_names) if a1 != a2])
 
-if os.path.exists("figures/results.csv"):
-    os.remove("figures/results.csv")
-file = open("figures/results.csv", "w")
-file.write("agent1,agent2,iswin,item\n")
-file.close()
-for epoch in tqdm(range(8)):
-    for first, second in agents_couple:
-        os.system(f"python pwArgumentAgent.py {first} {second}")
+# if os.path.exists("figures/results.csv"):
+#     os.remove("figures/results.csv")
+# file = open("figures/results.csv", "w")
+# file.write("agent1,agent2,iswin,item\n")
+# file.close()
+# for epoch in tqdm(range(8)):
+#     for first, second in agents_couple:
+#         os.system(f"python pwArgumentAgent.py {first} {second}")
 
 
 results = pd.read_csv('figures/results.csv')
@@ -42,12 +42,16 @@ for agent in player_names:
 fig = px.line(df, x="epoch", y="cumulative_wins", color="agent1", template="plotly_white", title="Number of wins per agent")
 fig.write_image("figures/cumulative_wins_per_agent.png")
 
+df = df.groupby(["epoch","agent1"])["cumulative_wins"].max().reset_index()
+
 df = pd.DataFrame()
 
 for item in item_names:
     temp = results[results.item == item]
     temp["cumulative_wins"] = temp.iswin.cumsum()
     df = pd.concat([df, temp[["cumulative_wins", "epoch", "item"]]])
+
+df = df.groupby(["epoch","item"])["cumulative_wins"].max().reset_index()
 
 fig = px.line(df, x="epoch", y="cumulative_wins", color="item", template="plotly_white", title="Number of wins per item")
 fig.write_image("figures/cumulative_wins_per_item.png")
