ef78ffe80bbdd57aab8c8fec640a8fede3e79377
alexfrst <alex.forest1026@gmail.com>
2022-04-15 21:43:15 +0200

    chore: updated graphs

--- pwArgumentAgentBatch.py
+++ pwArgumentAgentBatch.py
@@ -20,18 +20,18 @@ item_names = [
 
 agents_couple = ([(a1,a2) for a1, a2 in product(player_names, player_names) if a1 != a2])
 
-# if os.path.exists("figures/results.csv"):
-#     os.remove("figures/results.csv")
-# file = open("figures/results.csv", "w")
-# file.write("agent1,agent2,iswin,item\n")
-# file.close()
-# for epoch in tqdm(range(8)):
-#     for first, second in agents_couple:
-#         os.system(f"python pwArgumentAgent.py {first} {second}")
+if os.path.exists("figures/results.csv"):
+    os.remove("figures/results.csv")
+file = open("figures/results.csv", "w")
+file.write("agent1,agent2,iswin,item\n")
+file.close()
+for epoch in tqdm(range(8)):
+    for first, second in agents_couple:
+        os.system(f"python pwArgumentAgent.py {first} {second}")
 
 
 results = pd.read_csv('figures/results.csv')
-results["epoch"] = [i // 10 for i in range(len(results))]
+results["epoch"] = [i // 12 for i in range(len(results))]
 
 df = pd.DataFrame()
 for agent in player_names:
